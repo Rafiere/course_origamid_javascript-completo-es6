b5a028b6c11103c05283047b1e218b965cbdf353
Rafiere <rafapgbr@gmail.com>
2022-12-05 20:49:56 -0300

    feat: adding scripts for lesson 201.

---
+++ exercises/201-variaveis/script.js
@@ -0,0 +1,14 @@
+//Declarar uma variável com o seu nome:
+let nome = "Rafa";
+
+// Declarar uma variável com a sua idade:
+let idade = 21;
+
+// Declarar uma variável com a sua comida favorita e não atribuir valor:
+let comidaFavorita;
+
+//Atribuir valor a sua comida favorita:
+comidaFavorita = "Arroz e Feijão";
+
+//Declarar 5 variáveis diferentes sem valores:
+let var1, var2, var3, var4, var5;
