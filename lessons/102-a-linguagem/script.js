8c4982dfd53e7bb78c2b75f6bdd8cc7c51d12d2e
Rafiere <rafapgbr@gmail.com>
2022-12-05 20:50:26 -0300

    feat: adding scripts for lesson 102.

---
+++ lessons/102-a-linguagem/script.js
@@ -0,0 +1,11 @@
+/* O JavaScript é uma linguagem de programação interpretada. Ela não precisa ser compilada para ser executada. */
+
+/* O JavaScript pode ser utilizado no front-end, para a manipulação do DOM, a comunicação assíncrona com o back-end e várias outras coisas, pode ser usado no back-end, para a comunicação com o banco de dados, a manipulação de arquivos e outros, através do Node, para jogos e várias outros modos. */
+
+/* Abaixo, estamos criando uma função que recebe dois parâmetros. Ao término dessa função, ela retornará o valor da soma entre esses dois parâmetros recebidos. */
+
+function somar(a, b) {
+  return a + b;
+}
+
+somar(2, 4);
