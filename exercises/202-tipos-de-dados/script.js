02787eafc353419a22612f73be728940dba373c4
Rafiere <rafapgbr@gmail.com>
2022-12-05 20:50:02 -0300

    feat: adding scripts for lesson 202.

---
+++ exercises/202-tipos-de-dados/script.js
@@ -0,0 +1,20 @@
+/* Declare uma variável contendo uma string */
+var pais = "Brasil";
+
+/* Declare uma variável contendo um número dentro de uma string. */
+var ano = "2022";
+
+/* Declare uma variável com a sua idade. */
+var idade = 21;
+
+/* Declare duas variáveis, uma com o seu nome e outra com o seu sobrenome, e some as mesmas. */
+var nome = "Testando";
+var sobrenome = "Testador";
+console.log(nome + " " + sobrenome);
+console.log(`${nome} ${sobrenome}`);
+
+/* Coloque a seguinte frase em uma variável: "It's time". */
+var frase = "It's time";
+
+/* Verifique o tipo da variável que contém o seu nome. */
+console.log(typeof nome);
