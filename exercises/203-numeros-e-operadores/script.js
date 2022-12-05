d849706330645f5aaa3aeb47e89832a4c6a6277b
Rafiere <rafapgbr@gmail.com>
2022-12-05 20:50:07 -0300

    feat: adding scripts for lesson 203.

---
+++ exercises/203-numeros-e-operadores/script.js
@@ -0,0 +1,15 @@
+//Qual é o resultado da seguinte expressão?
+var total = 10 + (5 * 2) / 2 + 20; //35
+
+//Crie duas expressões que retornem NaN.
+var expressao1 = "Teste" - 5,
+  expressao2 = +"33" + "abc";
+
+//Somar a string "200" com o número "50" e retornar "250".
+var valor1 = "200";
+var numero1 = 50;
+var total = +valor1 + numero1; //250
+
+//Incremente o número 5 e retorne o seu valor incrementado.
+var numero2 = 5;
+console.log(++numero2);
