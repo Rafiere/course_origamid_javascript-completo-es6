c8d534885a84b2e0e7a748d6ce5ac967f7dfb664
Rafiere <rafapgbr@gmail.com>
2022-12-08 21:11:52 -0300

    feat: adding material for exercise 207

---
+++ exercises/207-tudo-e-objeto/script.js
@@ -0,0 +1,19 @@
+/* Nomeie três propriedades ou métodos de strings. */
+
+// fixed
+// length
+// slice
+
+/* Nomeie 5 propriedades ou métodos de elementos do DOM. */
+
+var btn = document.querySelector(".btn");
+
+// addEventListener
+// appendChild
+// id
+// innerHtml
+// outerHtml
+
+/* Busque na Web um objeto (método) capaz de interagir com o clipboard. Clipboard é a parte do computador que lida com o "CTRL C" + "CTRL V". */
+
+// Existe o "clipboard.js".
